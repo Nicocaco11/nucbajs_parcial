bc0534d00ccb8070a034fe8f20d6f225f22fefa2
Nicocaco11 <nicogabrielvidela11@gmail.com>
2023-12-05 21:25:52 -0300

    Terminado diccionario de productos.

---
+++ js/products.js
@@ -0,0 +1,86 @@
+const products = [
+    {
+        id: 1,
+        nombre: "Geforce GT 210 MSI",
+        precio: 83599.99,
+        category: "graphics",
+        img: "../src/products/Graphics/video-geforce-gt-210-msi-1gb-ddr3-0.jpg"
+    },
+    {
+        id: 2,
+        nombre: "Geforce GTX 1630 4GB Ventus",
+        precio: 213130.01,
+        category: "graphics",
+        img: "../src/products/Graphics/video-geforce-gtx-1630-4gb-ventus-xs-oc-0.jpg"
+    },
+    {
+        id: 3,
+        nombre: "Geforce GTX 1650 4GB Asus",
+        precio: 255000.03,
+        category: "graphics",
+        img: "../src/products/Graphics/video-geforce-gtx-1650-4gb-asus-dual-0.jpg"
+    },
+    {
+        id: 4,
+        nombre: "Geforce GTX 1660 6GB GigaByte",
+        precio: 566549.99,
+        category: "graphics",
+        img: "../src/products/Graphics/video-geforce-gtx-1660-6gb-gigabyte-oc-nv-0.jpg"
+    },
+    {
+        id: 5,
+        nombre: "Geforce RTX 3060TI 8GB PNY Verto Dual Fan",
+        precio: 588209.96,
+        category: "graphics",
+        img: "../src/products/Graphics/video-geforce-rtx-3060-ti-8gb-pny-verto-dual-fan-0.jpg"
+    },
+    {
+        id: 6,
+        nombre: "Geforce RTX 4060 8GB MSI Gaming",
+        precio: 619129.98,
+        category: "graphics",
+        img: "../src/products/Graphics/video-geforce-rtx-4060-8gb-msi-gaming-x-0.jpg"
+    },
+    {
+        id: 7,
+        nombre: "Micro AMD Ryzen 5 4600G",
+        precio: 198499.99,
+        category: "processors",
+        img: "../src/products/Processors/micro-amd-ryzen-5-4600g-cvideo-0.jpg"
+    },
+    {
+        id: 8,
+        nombre: "Micro AMD Ryzen 5 5600G",
+        precio: 238600.03,
+        category: "processors",
+        img: "../src/products/Processors/micro-amd-ryzen-5-5600g-0.jpg"
+    },
+    {
+        id: 9,
+        nombre: "Micro AMD Ryzen 5 7600G",
+        precio: 35200.03,
+        category: "processors",
+        img: "../src/products/Processors/micro-amd-ryzen-5-7600-cvideo-ccooler-am5-0.jpg"
+    },
+    {
+        id: 10,
+        nombre: "Memoria SODIMM 8GB DDR4 3200 HIKVISION",
+        precio: 27122.16,
+        category: "ram",
+        img: "../src/products/RAM/memoria-sodimm-8gb-ddr4-3200-hikvision-0.jpg",
+    },
+    {
+        id: 11,
+        nombre: "Memoria SODIMM 8GB DDR4 3200 ADATA PREMIER",
+        precio: 29609.97,
+        category: "ram",
+        img: "../src/products/RAM/memoria-sodimm-8gb-ddr4-3200-adata-premier-0.jpg",
+    },
+    {
+        id: 12,
+        nombre: "Memoria SODIMM 32GB DDR5 4800 ADATA",
+        precio: 195549.99,
+        category: "ram",
+        img: "../src/products/RAM/memoria-sodimm-32gb-ddr5-4800-adata-0.jpg",
+    }
+]
