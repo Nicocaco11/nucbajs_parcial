12169588f5350fe4318e36e1970ddf415a5797bd
Nicocaco11 <nicogabrielvidela11@gmail.com>
2023-12-24 16:15:55 -0300

    Pàgina web terminada

--- js/main.js
+++ js/main.js
@@ -19,3 +19,36 @@ window.addEventListener("resize", () =>{
 
 })
 
+document.getElementById("form__contact").addEventListener("submit", function (event) {
+
+    document.getElementById("nombreError").textContent = ""
+    document.getElementById("correoError").textContent = ""
+    document.getElementById("descripcionError").textContent = ""
+
+    let nombre = document.getElementById("nombre").value.trim()
+    let correo = document.getElementById("correo").value.trim()
+    let descripcion = document.getElementById("descripcion").value.trim()
+
+    if (nombre === "") {
+        document.getElementById("nombreError").textContent = "Ingrese un nombre"
+        event.preventDefault()
+    }
+
+    if (correo === "") {
+        document.getElementById("correoError").textContent = "Ingrese un correo"
+        event.preventDefault()
+    } else if (!isValidEmail(correo)) {
+        document.getElementById("correoError").textContent = "Ingrese un correo válido"
+        event.preventDefault()
+    }
+
+    if (descripcion === "") {
+        document.getElementById("descripcionError").textContent = "Ingrese un mensaje"
+        event.preventDefault()
+    }
+})
+
+function isValidEmail(email) {
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    return emailRegex.test(email)
+}
